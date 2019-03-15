LairdCP/igconfd

ig60: port services to python3
Bug 14099

Makefile
@@ -1,4 +1,5 @@
-IGCONFD_EGG = dist/igconfd-1.0-py2.7.egg
+TARGET_PYTHON_VERSION := $$(find $(TARGET_DIR)/usr/lib -maxdepth 1 -type d -name python* -printf "%f\n" | egrep -o '[0-9].[0-9]')
+IGCONFD_EGG = dist/igconfd-1.0-py$(TARGET_PYTHON_VERSION).egg
 IGCONFD_PY_SRCS = __main__.py gattsvc.py wifisvc.py
 IGCONFD_PY_SETUP = setup.py
 

__main__.py
@@ -1,21 +1,26 @@
 import dbus, dbus.service, dbus.exceptions
-import sys, signal
+import signal
 from syslog import syslog, openlog
 from dbus.mainloop.glib import DBusGMainLoop
 import systemd
 import systemd.daemon
 import devmngr
 
-try:
-  from gi.repository import GObject
-except ImportError:
-  import gobject as GObject
+import sys
+PYTHON3 = sys.version_info >= (3, 0)
+if PYTHON3:
+    from gi.repository import GObject as gobject
+    from gi.repository import GLib as glib
+else:
+    import gobject
 
 def main():
-    GObject.threads_init()
+    gobject.threads_init()
     dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
-
-    mainloop = GObject.MainLoop()
+    if PYTHON3:
+        mainloop = glib.MainLoop()
+    else:
+        mainloop = gobject.MainLoop()
 
     device_manager = devmngr.DeviceManager()
 

LairdCP/igupd

Makefile
@@ -1,5 +1,6 @@
 PYTHON ?= /usr/bin/python
-IGUPD_EGG = dist/igupd-1.0-py2.7.egg
+TARGET_PYTHON_VERSION := $$(find $(TARGET_DIR)/usr/lib -maxdepth 1 -type d -name python* -printf "%f\n" | egrep -o '[0-9].[0-9]')
+IGUPD_EGG = dist/igupd-1.0-py$(TARGET_PYTHON_VERSION).egg
 IGUPD_PY_SRCS = __main__.py swupd.py upsvc.py somutil.py resumetimer.py swuclient.py usbupd.py
 IGUPD_PY_SETUP = setup.py
 

__main__.py
@@ -1,10 +1,17 @@
 import dbus, dbus.service, dbus.exceptions
-import sys, signal
+import signal
 from syslog import syslog, openlog
 from dbus.mainloop.glib import DBusGMainLoop
-import gobject
 import swupd
 import random
+
+import sys
+PYTHON3 = sys.version_info >= (3, 0)
+if PYTHON3:
+    from gi.repository import GObject as gobject
+    from gi.repository import GLib as glib
+else:
+    import gobject
 
 # Global loop object
 loop = None
@@ -13,7 +20,10 @@ def main():
     # Initialize a main loop
     DBusGMainLoop(set_as_default=True)
     gobject.threads_init()
-    loop = gobject.MainLoop()
+    if PYTHON3:
+        loop = glib.MainLoop()
+    else:
+        loop = gobject.MainLoop()
 
     # Declare a name where our service can be reached
     try: